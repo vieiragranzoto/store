62205d60e0edf4e5f3710cf0d277f95de15f5328
vieiragranzoto <vieiragranzoto@gmail.com>
2023-02-28 20:52:38 -0300

    Add subprodutos com tipo de herança joined

---
+++ src/main/java/br/com/alura/store/model/Informatica.java
@@ -0,0 +1,36 @@
+package br.com.alura.store.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "informatica")
+public class Informatica extends Produto {
+
+	private String marca;
+	private Integer modelo;
+
+	public Informatica() {
+	}
+
+	public Informatica(String marca, Integer modelo) {
+		this.marca = marca;
+		this.modelo = modelo;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public Integer getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(Integer modelo) {
+		this.modelo = modelo;
+	}
+}
